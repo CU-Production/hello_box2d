12507f9d1ba608f2bdf11bceaa3a473d93f4960d
THISISAGOODNAME <yangyj19931231@gmail.com>
2025-01-02 14:59:19 +0800

    Update box2d to v3.0.0

--- src/hellobox2d/main.cpp
+++ src/hellobox2d/main.cpp
@@ -6,46 +6,45 @@ int main() {
     // world
     b2Vec2 gravity(0.0f, -10.0f);
 
-    b2World world(gravity);
+    b2WorldDef worldDef = b2DefaultWorldDef();
+    worldDef.gravity = gravity;
+
+    b2WorldId worldId = b2CreateWorld(&worldDef);
 
     // ground
-    b2BodyDef groundBodyDef;
-    groundBodyDef.position.Set(0.0f, -10.f);
+    b2BodyDef groundBodyDef = b2DefaultBodyDef();
+    groundBodyDef.position = {0.0f, -10.f};
 
-    b2Body* groundBody = world.CreateBody(&groundBodyDef);
+    b2BodyId groundId = b2CreateBody(worldId, &groundBodyDef);
 
-    b2PolygonShape groundBox;
-    groundBox.SetAsBox(50.0f, 10.0f);
+    b2Polygon groundBox = b2MakeBox(50.0f, 10.0f);
 
-    groundBody->CreateFixture(&groundBox, 0.0f);
+    b2ShapeDef groundShapeDef = b2DefaultShapeDef();
+    b2CreatePolygonShape(groundId, &groundShapeDef, &groundBox);
 
     // dynamic body
-    b2BodyDef bodyDef;
+    b2BodyDef bodyDef = b2DefaultBodyDef();
     bodyDef.type = b2_dynamicBody;
-    bodyDef.position.Set(0.0f, 4.0f);
+    bodyDef.position = {0.0f, 4.0f};
 
-    b2Body* body = world.CreateBody(&bodyDef);
+    b2BodyId bodyId = b2CreateBody(worldId, &bodyDef);
 
-    b2PolygonShape dynamicBox;
-    dynamicBox.SetAsBox(1.0f, 1.0f);
+    b2Polygon dynamicBox = b2MakeBox(1.0f, 1.0f);
 
-    b2FixtureDef fixtureDef;
-    fixtureDef.shape = &dynamicBox;
-    fixtureDef.density = 1.0f;
-    fixtureDef.friction = 0.3f;
+    b2ShapeDef shapeDef = b2DefaultShapeDef();
+    shapeDef.density = 1.0f;
+    shapeDef.friction = 0.3f;
 
-    body->CreateFixture(&fixtureDef);
+    b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
 
     // step
     const float timeStep = 1.0f / 60.0f;
+    const int subStepCount = 4;
 
-    const int32 velocityIterations = 6;
-    const int32 positionIterations = 2;
-
-    for (int32 i = 0; i < 200; i++) {
-        world.Step(timeStep, velocityIterations, positionIterations);
-        b2Vec2 position = body->GetPosition();
-        float angle = body->GetAngle();
-        printf("%4.2f %4.2f %4.2f\n", position.x, position.y, angle);
+    for (int i = 0; i < 200; i++) {
+        b2World_Step(worldId, timeStep, subStepCount);
+        b2Vec2 position = b2Body_GetPosition(bodyId);
+        b2Rot angle = b2Body_GetRotation(bodyId);
+        printf("%4.2f %4.2f %4.2f\n", position.x, position.y, b2Rot_GetAngle(angle));
     }
 }
